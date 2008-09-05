0cf0c809d43fca63804fe43a06eb8e4a295b8ed8
keo <info@binarysec.com>
2008-09-05 14:06:11 +0000

    interface d'admin ok

--- agg/admin_html.php
+++ agg/admin_html.php
@@ -24,16 +24,57 @@
 
 class admin_html extends wf_agg {
 
-	var $a_core_route;
+	private $a_core_route;
+	private $title;
+	private $subtitle;
 
 	public function loader($wf) {
 		$this->wf = $wf;
 		$this->a_core_route = $this->wf->core_route();
 	}
 
+	public function set_title($title) {
+		$this->title = $title;
+	}
+
+	public function set_subtitle($subtitle) {
+		$this->subtitle = $subtitle;
+	}
+
+	private function get_subroutes($link, $recur = false) {
+		$dir = explode("/", rtrim($link, '/'));
+		$nav = &$this->a_core_route->routes;
+		for($i=1; $i<count($dir); $i++) {
+			if(isset($nav[0][$dir[$i]]))
+				$nav = &$nav[0][$dir[$i]];
+			else
+				return(array());
+		}
+		return($this->list_routes(&$nav[0], $link, $recur));
+	}
+
+	private function list_routes($routes, $base = '', $recur = false, $subroutes = array()) {
+		foreach($routes as $name => $route) {
+			foreach($route as $i => $infos) {
+				if(isset($infos[0])) {
+					$b = $base.'/'.$name;
+ 					$subroutes[$b] = ($infos[2] == WF_ROUTE_ACTION) ? $infos[5] : $infos[4];
+				}
+				else if($recur)
+					$this->list_routes(&$infos, $base.'/'.$name, $recur, &$subroutes);
+			}
+		}
+		return($subroutes);
+	}
+
 	public function rendering($body) {
 		$tpl = new core_tpl($this->wf);
-		$tpl->set('html_body', $body);
+		$tpl->set('admin_sidemenu', $this->get_subroutes('/admin'));
+		$tpl->set('admin_topmenu', '');
+		$tpl->set('admin_date', date('M, m Y'));
+		$tpl->set('admin_title', $this->title);
+		$tpl->set('admin_subtitle', $this->subtitle);
+		$tpl->set('admin_body', $body);
 		$this->wf->core_html()->rendering($tpl->fetch('admin/main'));
 	}
 
