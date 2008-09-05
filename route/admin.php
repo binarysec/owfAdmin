7f73a5c11b7691d1529cf6be19e9db333d09e9bb
keo <info@binarysec.com>
2008-09-05 07:59:46 +0000

    git-svn-id: file:///home/svn/nat/admin/branches/1_0@7 695193a4-7260-4023-beb5-47841f34d476

--- route/admin.php
+++ route/admin.php
@@ -2,9 +2,26 @@
 
 class wfr_admin_admin extends wf_route_request {
 
+	private $a_core_route;
+
 	public function __construct($wf) {
 		$this->wf = $wf;
+		$this->a_core_route = $this->wf->core_route();
 
+		echo '<pre>';
+
+		
+		$this->nav('', &$this->a_core_route->routes[0]);
+print_r(&$this->a_core_route->routes[0]);
+		
+
+		exit(0);
+	}
+
+	private function nav($base, $routes, $l=0) {
+		foreach($routes as $i => $route) {
+			//
+		}
 	}
 
 	public function show_admin() {
