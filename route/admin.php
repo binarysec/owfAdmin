432db0df7190d5af72c85f62c073b043086d30a9
keo <info@binarysec.com>
2008-12-29 12:13:57 +0000

    ne crée pas d'index pour les routes redirection
    git-svn-id: file:///home/svn/nat/admin/branches/1_0@80 695193a4-7260-4023-beb5-47841f34d476

--- route/admin.php
+++ route/admin.php
@@ -17,10 +17,12 @@ class wfr_admin_admin extends wf_route_request {
 		/* menu global d'administration */
  		$routes = &$this->wf->core_route()->routes[0]["admin"][0];
  		foreach($routes as $route) {
-			$mod = $this->wf->modules[$route[1][1]][8];
-			if(method_exists($mod, 'get_index')) {
-				$html .= '<h1>'.$route[1][5].'</h1>';
-				$html .= $mod->get_index();
+			if($route[1][2] == WF_ROUTE_ACTION) {
+				$mod = $this->wf->modules[$route[1][1]][8];
+				if(method_exists($mod, 'get_index')) {
+						$html .= '<h1>'.$route[1][5].'</h1>';
+					$html .= $mod->get_index();
+				}
 			}
 		}
 
