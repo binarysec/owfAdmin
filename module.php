b8921e7c42915639a503686d48bcf3b41f81e559
mykiimike <mv@verman.fr>
2011-06-16 10:25:31 +0000

    Correcting template Adaptating template

--- module.php
+++ module.php
@@ -7,8 +7,8 @@ class wfm_admin extends wf_module {
 	
 	public function get_name() { return("admin"); }
 	public function get_description()  { return("OWF Native Administration module"); }
-	public function get_banner()  { return("admin/1.3.0"); }
-	public function get_version() { return("1.3.0"); }
+	public function get_banner()  { return("admin/1.3.1"); }
+	public function get_version() { return("1.3.1"); }
 	public function get_authors() { return(array("Michael VERGOZ")); }
 	public function get_depends() { return(NULL); }
 	
