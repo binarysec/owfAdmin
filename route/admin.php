bb031dbbe9f2f6b888e051e405dfa17a2a15af0e
Lazarus974 <ol@verman.fr>
2013-08-14 08:04:25 +0000

    Added and updated translations

--- route/admin.php
+++ route/admin.php
@@ -6,17 +6,14 @@ class wfr_admin_admin extends wf_route_request {
 	}
 
 	public function show() {
-		/* get primary content */
-		$tpl = new core_tpl($this->wf);
-		$in = array(			
-		);	 
-		$tpl->set_vars($in);
 		/*$this->wf->admin_html()->set_backlink(
 			$this->wf->linker("/session/logout"),
 			"Logout",
 			"delete",
 			true
 		);*/
+		
+		$tpl = new core_tpl($this->wf);
 		$this->wf->admin_html()->renderlinks(array(
 			"body" => $tpl->fetch('admin/index'),
 			"backurl" => "foo",
